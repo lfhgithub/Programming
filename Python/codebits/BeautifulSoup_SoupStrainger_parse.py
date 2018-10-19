4bdf2ee70e474b68bebfa964adc5ccb72f095712
lfhgithub <lhollas4@gmail.com>
2018-10-19 15:22:15 -0500

    Create BeautifulSoup_SoupStrainger_parse.py
    Code bit how to parse with SoupStrainer and BeautifulSoup

---
+++ Python/codebits/BeautifulSoup_SoupStrainger_parse.py
@@ -0,0 +1,14 @@
+# Using SoupStrainer to parse selectively
+
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+import urllib
+import re
+
+
+start = time.clock()
+url = "http://someplace.com"
+html = urllib.urlopen(url).read()
+product = SoupStrainer('div',{'id': 'products_list'})
+soup = BeautifulSoup(html,parseOnlyThese=product)
+for a in soup.findAll('a',{'title':re.compile('.+') }):
+      print a.string
